3e978a34c298d879c217eb175bb72209a673ab24
ElXirosa <benjamin.lagier@yahoo.com>
2026-05-16 20:54:41 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -14,7 +14,6 @@ export async function rechercherVilles(nomVille) {
 
 export async function fetchMeteo(latitude, longitude, date){
     try{
-        console.log(`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date}&end_date=${date}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_max,precipitation_sum,snowfall_sum,sunshine_duration,wind_direction_10m_dominant,shortwave_radiation_sum,cloud_cover_mean,relative_humidity_2m_mean,pressure_msl_mean,wind_speed_10m_max,wind_gusts_10m_max,dew_point_2m_mean,surface_pressure_mean`);
         const response = await fetch(`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date}&end_date=${date}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_max,precipitation_sum,snowfall_sum,sunshine_duration,wind_direction_10m_dominant,shortwave_radiation_sum,cloud_cover_mean,relative_humidity_2m_mean,pressure_msl_mean,wind_speed_10m_max,wind_gusts_10m_max,dew_point_2m_mean,surface_pressure_mean`);
         const data = await response.json()
         return data ?? [];
